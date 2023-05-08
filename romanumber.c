d68e2368c11be8ac7d4d7b731399130833c8c0f5
Sriharsha-AVC-Baseline <83340788+Sriharsha-AVC-Baseline@users.noreply.github.com>
2023-05-08 20:27:16 +0530

    Update romanumber.c

--- romanumber.c
+++ romanumber.c
@@ -6,48 +6,48 @@
 #include<string.h>
 #include<stdlib.h>
 
-int values(char c)
+int values(char value)
 {
-    char val[] = {'l','V','X','L','C','D','M'};
-    int numval[]={1,5,10,50,100,500,1000};
-    int i=0;
-    for(i=0;i<7;i++)
+    char RomanValues[] = {'l','V','X','L','C','D','M'};
+    int NumericalValues[]={1,5,10,50,100,500,1000};
+    int iterator=0;
+    for(iterator = 0 ; iterator < 7 ; iterator++)
     {
-        if(c==val[i])
+        if(value == RomanValues[iterator])
         {
-            return numval[i];
+            return NumericalValues[iterator];
         }
     }
 }
 
-int roman_to_number(char* rmn)
+int roman_to_number(char* roman_number)
 {
-   int i=0,len=strlen(rmn);
-   int number=values(rmn[0]),singles=0,recent=values(rmn[0]);
-   for(i=1;i<len-1;i++)
+   int iterator=0,length=strlen(roman_number);
+   int number=values(roman_number[0]);
+   int singles=0;
+   int recent=values(roman_number[0]);
+   for(iterator = 1;iterator < length-1; iterator++)
    {
-        int prev=values(rmn[i-1]);
-            if(prev < values(rmn[i]))
+        int previous=values(roman_number[iterator-1]);
+            if(previous < values(roman_number[iterator]))
             {
-                number=number -prev +(values(rmn[i])-prev);
+                number = number - previous + (values(roman_number[iterator]) - previous);
             }
             else
             {
-                number=values(rmn[i])+number;
+                number = values(roman_number[iterator]) + number;
             }
-
-            printf("%d\t",number);
         
-      recent=values(rmn[i]);
+      recent=values(roman_number[iterator]);
    }
    return number;
 }
 int main()
 {
-    char strng[100];
-    fgets(strng,100,stdin);
+    char string[100];
+    fgets(string,100,stdin);
     
     
-    printf("%d\n",roman_to_number(strng));
+    printf("Numerical value is %d\n",roman_to_number(string));
     
 }
