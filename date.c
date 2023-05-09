c3f2e53514f292e82c7c08557cd64bc38c421b6b
Sriharsha-AVC-Baseline <83340788+Sriharsha-AVC-Baseline@users.noreply.github.com>
2023-05-09 09:22:27 +0530

    Update date.c

--- date.c
+++ date.c
@@ -4,33 +4,35 @@
 #include<string.h>
 #include<stdlib.h>
 
-void get_date(char *str)
+void get_date(char *string)
 {
-	int day,i,number=0,temp,j=0,date[3];
+	int day,iterator1,number=0,temporary;
+	int iterator2 = 0;
+	int date[3];
 	char month[20][12]={"January","February","March","April","May","June","July","August","September","October","November","December"};
-	int length=strlen(str)-1;
-	for(i=0;i<length;i++)
+	int length=strlen(string)-1;
+	for(iterator1 = 0; iterator1 < length; iterator1++)
 	{
-		if(str[i]>=48&&str[i]<=57)
+		if(string[iterator1] >= 48 && string[iterator1] <= 57)
 		{
-			while(str[i]>=48&&str[i]<=57)
+			while(string [iterator1] >= 48 && string [iterator1] <= 57)
 			{
-				temp=str[i]-48;
-				number=number*10+temp;
-				i++;
+				temporary=string[iterator1]-48;
+				number=number*10+temporary;
+				iterator1++;
 			}
 		}
-		if(str[i]==' '||str[i]=='/'||str[i]=='-')
+		if(string[iterator1]==' '||string[iterator1]=='/'||string[iterator1]=='-')
 		{
-			date[j]=number;
-			j++;
+			date[iterator2]=number;
+			iterator2++;
 			number=0;
 			continue;
 		}
 		
 	} 
-	date[j]=number;
-	if(date[0]>31 || date[1]>12 )
+	date[iterator2]=number;
+	if(date[0]>31 || date[1]>12 ||(date[1]==2&&date[0]>29) )
 	{
 		printf("Error");
 		exit(0);
@@ -41,7 +43,7 @@ void get_date(char *str)
 int main()
 {
 	int data;
-	char str[200];
-	fgets(str,200,stdin);
-	get_date(str);	
+	char string[200];
+	fgets(string,200,stdin);
+	get_date(string);	
 }
